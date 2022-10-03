8d03aa9ba8134a644d2e66ef0ce59b73a6b19ed3
Emaje Graves <emajeg23@gmail.com>
2022-10-02 22:07:57 -0500

    Created Notes class

---
+++ CS401app/Notes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherApp
+{
+    class Notes
+    {
+        public static int NoteID { get; set; }     
+    }
+}

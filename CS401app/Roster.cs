dfd77019ab80addc44c661d794abeda05c9b6dde
Emaje Graves <emajeg23@gmail.com>
2022-10-26 19:26:50 -0500

    Created Roster Class

---
+++ CS401app/Roster.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherApp
+{
+    class Roster
+    {
+        public static int RosterId { get; set; }  
+        public static string RosterData { get; set; }
+    }
+}

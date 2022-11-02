8e32a6afd2687aabe7355f5a7c2589092bfa7a61
brakia12 <brakiajackson55@gmail.com>
2022-11-02 16:20:25 -0500

    Updated Announcements form

--- CS401app/Forms/AnnouncementsForm.Designer.cs
+++ CS401app/Forms/AnnouncementsForm.Designer.cs
@@ -29,75 +29,157 @@ namespace TeacherApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.createAnnBTN = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
             this.AnnouncementsTXT = new System.Windows.Forms.TextBox();
-            this.homebtn = new System.Windows.Forms.Button();
-            this.modifyAnnouncement = new System.Windows.Forms.Button();
+            this.subjectcomboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.editbtn = new System.Windows.Forms.Button();
+            this.modifyPNL = new System.Windows.Forms.Panel();
+            this.saveBtn = new System.Windows.Forms.Button();
+            this.refreshBtn = new System.Windows.Forms.Button();
+            this.createNewLBL = new System.Windows.Forms.LinkLabel();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.modifyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modifyPNL.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // createAnnBTN
-            // 
-            this.createAnnBTN.Location = new System.Drawing.Point(468, 401);
-            this.createAnnBTN.Name = "createAnnBTN";
-            this.createAnnBTN.Size = new System.Drawing.Size(155, 23);
-            this.createAnnBTN.TabIndex = 6;
-            this.createAnnBTN.Text = "Create Announcement";
-            this.createAnnBTN.UseVisualStyleBackColor = true;
-            this.createAnnBTN.Click += new System.EventHandler(this.createAnnBTN_Click);
             // 
             // AnnouncementsTXT
             // 
             this.AnnouncementsTXT.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.AnnouncementsTXT.HideSelection = false;
-            this.AnnouncementsTXT.Location = new System.Drawing.Point(35, 36);
+            this.AnnouncementsTXT.Location = new System.Drawing.Point(43, 86);
             this.AnnouncementsTXT.Multiline = true;
             this.AnnouncementsTXT.Name = "AnnouncementsTXT";
             this.AnnouncementsTXT.ReadOnly = true;
-            this.AnnouncementsTXT.Size = new System.Drawing.Size(674, 311);
+            this.AnnouncementsTXT.Size = new System.Drawing.Size(674, 327);
             this.AnnouncementsTXT.TabIndex = 5;
             this.AnnouncementsTXT.TextChanged += new System.EventHandler(this.AnnouncementsTXT_TextChanged);
             // 
-            // homebtn
+            // subjectcomboBox
             // 
-            this.homebtn.Location = new System.Drawing.Point(287, 366);
-            this.homebtn.Name = "homebtn";
-            this.homebtn.Size = new System.Drawing.Size(127, 23);
-            this.homebtn.TabIndex = 7;
-            this.homebtn.Text = "Home";
-            this.homebtn.UseVisualStyleBackColor = true;
-            this.homebtn.Click += new System.EventHandler(this.homebtn_Click);
+            this.subjectcomboBox.FormattingEnabled = true;
+            this.subjectcomboBox.Location = new System.Drawing.Point(287, 48);
+            this.subjectcomboBox.Name = "subjectcomboBox";
+            this.subjectcomboBox.Size = new System.Drawing.Size(155, 21);
+            this.subjectcomboBox.TabIndex = 9;
+            this.subjectcomboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
-            // modifyAnnouncement
+            // label1
             // 
-            this.modifyAnnouncement.Location = new System.Drawing.Point(633, 401);
-            this.modifyAnnouncement.Name = "modifyAnnouncement";
-            this.modifyAnnouncement.Size = new System.Drawing.Size(83, 23);
-            this.modifyAnnouncement.TabIndex = 8;
-            this.modifyAnnouncement.Text = "Modify";
-            this.modifyAnnouncement.UseVisualStyleBackColor = true;
-            this.modifyAnnouncement.Click += new System.EventHandler(this.modifyAnnouncement_Click);
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(241, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Select:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // editbtn
+            // 
+            this.editbtn.Location = new System.Drawing.Point(32, 57);
+            this.editbtn.Name = "editbtn";
+            this.editbtn.Size = new System.Drawing.Size(118, 23);
+            this.editbtn.TabIndex = 11;
+            this.editbtn.Text = "Edit";
+            this.editbtn.UseVisualStyleBackColor = true;
+            // 
+            // modifyPNL
+            // 
+            this.modifyPNL.Controls.Add(this.saveBtn);
+            this.modifyPNL.Controls.Add(this.editbtn);
+            this.modifyPNL.Location = new System.Drawing.Point(199, 419);
+            this.modifyPNL.Name = "modifyPNL";
+            this.modifyPNL.Size = new System.Drawing.Size(381, 101);
+            this.modifyPNL.TabIndex = 12;
+            // 
+            // saveBtn
+            // 
+            this.saveBtn.Location = new System.Drawing.Point(208, 57);
+            this.saveBtn.Name = "saveBtn";
+            this.saveBtn.Size = new System.Drawing.Size(102, 23);
+            this.saveBtn.TabIndex = 12;
+            this.saveBtn.Text = "Save";
+            this.saveBtn.UseVisualStyleBackColor = true;
+            // 
+            // refreshBtn
+            // 
+            this.refreshBtn.Location = new System.Drawing.Point(448, 51);
+            this.refreshBtn.Name = "refreshBtn";
+            this.refreshBtn.Size = new System.Drawing.Size(75, 23);
+            this.refreshBtn.TabIndex = 13;
+            this.refreshBtn.Text = "Refresh";
+            this.refreshBtn.UseVisualStyleBackColor = true;
+            this.refreshBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // createNewLBL
+            // 
+            this.createNewLBL.AutoSize = true;
+            this.createNewLBL.Location = new System.Drawing.Point(330, 22);
+            this.createNewLBL.Name = "createNewLBL";
+            this.createNewLBL.Size = new System.Drawing.Size(63, 13);
+            this.createNewLBL.TabIndex = 14;
+            this.createNewLBL.TabStop = true;
+            this.createNewLBL.Text = "Create New";
+            this.createNewLBL.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.createNewLBL_LinkClicked);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.modifyToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 16;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // modifyToolStripMenuItem
+            // 
+            this.modifyToolStripMenuItem.Name = "modifyToolStripMenuItem";
+            this.modifyToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.modifyToolStripMenuItem.Text = "Modify";
+            this.modifyToolStripMenuItem.Click += new System.EventHandler(this.modifyToolStripMenuItem_Click);
             // 
             // AnnouncementsForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.modifyAnnouncement);
-            this.Controls.Add(this.homebtn);
-            this.Controls.Add(this.createAnnBTN);
+            this.ClientSize = new System.Drawing.Size(800, 532);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.createNewLBL);
+            this.Controls.Add(this.refreshBtn);
+            this.Controls.Add(this.modifyPNL);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.subjectcomboBox);
             this.Controls.Add(this.AnnouncementsTXT);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "AnnouncementsForm";
             this.Text = "Announcements";
+            this.modifyPNL.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button createAnnBTN;
         private System.Windows.Forms.TextBox AnnouncementsTXT;
-        private System.Windows.Forms.Button homebtn;
-        private System.Windows.Forms.Button modifyAnnouncement;
+        private System.Windows.Forms.ComboBox subjectcomboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button editbtn;
+        private System.Windows.Forms.Panel modifyPNL;
+        private System.Windows.Forms.Button refreshBtn;
+        private System.Windows.Forms.LinkLabel createNewLBL;
+        private System.Windows.Forms.Button saveBtn;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem modifyToolStripMenuItem;
     }
 }
--- CS401app/Forms/AnnouncementsForm.cs
+++ CS401app/Forms/AnnouncementsForm.cs
@@ -14,8 +14,8 @@ namespace TeacherApp
     {
         //initalize
         //HomeForm homeForm;
-        //CreateAnnouncements createAnnouncements;
-        //ModifyAnnouncements modifyAnnouncements;
+        createAnnouncements createAnnouncements;
+        ModifyAnnouncements modifyAnnouncements;
 
         public AnnouncementsForm()
         {
@@ -41,5 +41,117 @@ namespace TeacherApp
         {
 
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        //create new announcement will open
+        private void createNewLBL_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            if (null == createAnnouncements || createAnnouncements.IsDisposed)
+            {
+                createAnnouncements = new createAnnouncements();
+            }
+
+            createAnnouncements.ShowDialog();
+        }
+
+        private void ShowModify()
+        {
+            modifyPNL.Enabled = true;
+            modifyPNL.Visible = true;
+        }
+
+        private void HideModify()
+        {
+            modifyPNL.Enabled = false;
+            modifyPNL.Visible = false;
+        }
+
+        private void ValidateUserRole()
+        {
+            if (User.UserRole == "Teacher" || User.UserRole == "Admin")
+            {
+                HideModify();
+            }
+            else
+            {
+                modifyToolStripMenuItem.Visible = false;
+                createNewLBL.Visible = false;
+                HideModify();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           /* try
+            {
+                string noteName = notesComboBox.SelectedItem.ToString();
+                string sqlStr = "SELECT * FROM Notes WHERE userID = '" + User.UserId + "' And noteName = '" + noteName + "' ";
+                int rowsReturned = 0;
+                //Admin data table calling get function from dataBaseMGR
+                notesDataTable.Clear();
+                notesDataTable = dbMgrSQLite.getData(sqlStr, out rowsReturned);
+                if (rowsReturned > 0)
+                {
+                    foreach (DataRow dr in notesDataTable.Rows) // looping though rows
+                    {
+                        string notesName = dr["noteName"].ToString();
+                        string noteData = dr["noteData"].ToString();
+                        Notes.NoteID = Convert.ToInt32(dr["notesId"]);
+
+                        NotesTxtBox.Text = noteData.ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }*/
+        }
+
+        /*private void PopulateRosterSelectComboBoxData()
+        {
+            try
+            {
+                string sqlStr = "SELECT * FROM RosterTable WHERE UserID = '" + User.UserId + "' ";
+                int rowsReturned = 0;
+
+                rosterDataTable.Clear();
+                rosterDataTable = dataBaseMgr.getData(sqlStr, out rowsReturned);
+                if (rowsReturned > 0)
+                {
+                    foreach (DataRow dr in rosterDataTable.Rows)
+                    {
+                        string rosterName = dr["RosterName"].ToString();
+                        selectComboBox1.Items.Add(rosterName);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }*/
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            refreshBtn.Enabled = false;
+            subjectcomboBox.Enabled = true;
+            subjectcomboBox.Items.Clear();
+
+            //PopulateRosterSelectComboBoxData();
+        }
+
+        private void modifyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //ShowModify();
+            //createLinkLabel.Enabled = false;
+            //selectComboBox1.Enabled = false;
+        }
+
+
     }
 }

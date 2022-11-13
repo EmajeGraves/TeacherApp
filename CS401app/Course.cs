e8be4545c036ccab92cdf8878ef1ba38e87ceabf
Emaje Graves <emajeg23@gmail.com>
2022-11-13 10:29:33 -0600

    Changed name from roster to course

---
+++ CS401app/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeacherApp
+{
+    class Course
+    {
+        public static int CourseId { get; set; }  
+        
+
+        public static List<int> IdList = new List<int>();
+
+        public static List<int> CouseData = new List<int>();
+
+
+    }
+}

183b6b54c03ba038b4b9b0c679b5d4da590fb1e8
Emaje Graves <emajeg23@gmail.com>
2022-11-15 21:27:19 -0600

    Connected form to database

--- CS401app/Forms/createAnnouncements.cs
+++ CS401app/Forms/createAnnouncements.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TeacherApp.Forms;
 
 namespace TeacherApp
 {
@@ -14,86 +17,76 @@ namespace TeacherApp
     {
         // declaring variables
         public DatabaseMgrSQLite dataBaseMgr;
+        private DataTable rosterDataTable = new DataTable();
 
         public CreateAnnouncements()
         {
             InitializeComponent();
             dataBaseMgr = new DatabaseMgrSQLite();
-        }
 
-        private void label1_Click(object sender, EventArgs e)
-        {
-
+            PopulateSelectCourseId();
         }
 
         private void submitAnnouncementBtn_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Changes Saved");
             try
             {
-                // Saving text to variable
-                //string subject = subjectTXT.Text;
-                string announcement = announcementTXT.Text;
+                int courseId = Convert.ToInt32(selectComboBox.SelectedItem);
+                string subject = subjectTXT.Text;
+                string date = dateTimePicker.Text;
+                string announcement = announcementTXT.Text; 
+                string sqlStr = "INSERT INTO AnnouncementTable(courseID, userId, subject, date, announcementData) VALUES ('" + courseId + "','" + User.UserId + "','" + subject + "','" + date + "','" + announcement + "')";
 
-                // creting sqlite statement
-                //string sqlStr = "INSERT INTO Students ('subject', 'announcement') VALUES('" + subject + "', '" + announcement + "')";
-
-                // send data to data base
                 int rowsInserted = 0;
-                //rowsInserted = dataBaseMgr.putData(sqlStr);
+                rowsInserted = dataBaseMgr.putData(sqlStr);
 
-                // checking to make sure announcement was created
                 if (rowsInserted == 1)
-                {
-                    MessageBox.Show("Announcement Created!");
+                {  
+                    MessageBox.Show( "Announcement Sent");
+
+                    ClearTexts();
                     Close();
-                }
-                else
-                {
-                    MessageBox.Show("Error , Try Again");
                 }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-
             }
-            /*
-            //step 1: read and store data from file
-            string subject = subjectTXT.Text;
-            string announcement = announcementTXT.Text;
-
-            //step2 generate sql insert statement
-            string sqlStr = "INSERT INTO Students ('subject', 'announcement') VALUES('" + subject + "', '" + announcement + "')";
-
-            //step 3 send data to database using dbMgr
-            int rowsInserted = 0;
-            rowsInserted = dbMgr.putData(sqlStr);
-
-            //test 3 
-            MessageBox.Show("Inserted " + rowsInserted + " rows");
-
-            //step 4 display success or error
-
-            if (rowsInserted == 1)
-            {
-                MessageBox.Show("Data added Successfully");
-            }
-            else
-            {
-                MessageBox.Show("Error: Not added successfully");
-            }
-            */
         }
-
-        private void createAnnouncements_Load(object sender, EventArgs e)
-        {
-
-        }
-
         private void cancelBtn_Click(object sender, EventArgs e)
         {
             Close();
+        }
+        private void PopulateSelectCourseId()
+        {
+            try
+            {
+                string sqlStr = "SELECT * FROM CourseTable WHERE teacherId = '" + User.UserId + "' ";
+                int rowsReturned = 0;
+
+                rosterDataTable.Clear();
+                rosterDataTable = dataBaseMgr.getData(sqlStr, out rowsReturned);
+                if (rowsReturned > 0)
+                {
+                    foreach (DataRow dr in rosterDataTable.Rows)
+                    {
+                        int courseIds = Convert.ToInt32(dr["courseID"]);                
+              
+                        selectComboBox.Items.Add(courseIds);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        private void ClearTexts()
+        {
+            selectComboBox.Items.Clear();
+            subjectTXT.Clear();
+            announcementTXT.Clear();
         }
     }
 }
